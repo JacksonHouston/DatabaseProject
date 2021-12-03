e62b3ede412cbc950540a91578c175f27a678f56
JacksonHouston <Houston.jack07@gmail.com>
2021-12-02 20:29:38 -0500

    changed input to raw_input

--- Database.py
+++ Database.py
@@ -8,9 +8,9 @@ print("Connection established to: ",data)
 
 def Insert():
     print("To insert into the table, please provide the name, year released (YYYY-MM-DD), and description of the show.")
-    name = input("Enter name:")
-    date = input("Enter year released (YYYY-MM-DD):") #check which format is used in DB
-    description = input("Enter description:")
+    name = raw_input("Enter name:")
+    date = raw_input("Enter year released (YYYY-MM-DD):") #check which format is used in DB
+    description = raw_input("Enter description:")
 
     sqlInsert = "INSERT INTO TVshow (name, date, description) VALUES (" + name + ", " + date + ", "+ description + ")"
     cursor.execute(sqlInsert)
@@ -21,7 +21,7 @@ def Insert():
 
 def Select():
     print("To select on the table, please provide the attribute you'd like to view or use '*' to view all")
-    selection = input("Enter attribute seperated by a comma ',' :")
+    selection = raw_input("Enter attribute seperated by a comma ',' :")
     
     sqlSelect = "Select" + selection + "from TVshow"
     cursor.execute(sqlSelect)
@@ -33,9 +33,9 @@ def Select():
 
 def Update():
     print("To update on the table, please provide the attribute you'd like to update as well as the current value and new value.")
-    attribute = input("Enter attribute:")
-    curVal = input("Enter current value:")
-    newVal = input("Enter new value:")
+    attribute = raw_input("Enter attribute:")
+    curVal = raw_input("Enter current value:")
+    newVal = raw_input("Enter new value:")
 
     sqlUpdate = "UPDATE TVshow SET" + attribute +" = '" + curVal+"' WHERE" + attribute + "= '" + newVal +"'"
     cursor.execute(sqlUpdate)
@@ -46,8 +46,8 @@ def Update():
 
 def Delete():
     print("To delete on the table, please provide the attribute you'd like to delete as well as its current value.")
-    attribute = input("Enter attribute:")
-    curVal = input("Enter current value:")
+    attribute = raw_input("Enter attribute:")
+    curVal = raw_input("Enter current value:")
 
     sqlDelete = "DELETE FROM TVshow WHERE" + attribute +" = '" + curVal+"'"
     cursor.execute(sqlDelete)
@@ -56,7 +56,7 @@ def Delete():
     conn.commit()
     print(mycursor.rowcount, "record(s) deleted.")
 
-action = input("What would you like to do? (insert, select, update, delete)")
+action = raw_input("What would you like to do? (insert, select, update, delete)")
 if action == "insert":
     Insert()
 elif action == "select":
